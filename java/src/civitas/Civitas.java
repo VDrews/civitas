abfe4a13a292fd5b49f20b8aeecc1476cc24fb19
jmpobma <jmpobma@ei142087>
2019-09-18 16:40:58 +0200

    Start

---
+++ java/src/civitas/Civitas.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package civitas;
+
+enum TipoCasilla {CALLE, SORPRESA, JUEZ, IMPUESTO, DESCANSO}
+enum Sorpresa {IRCARCEL, IRCASILLA, PAGARCOBRAR, PORCASAHOTEL, PORJUGADOR, SALIRCARCEL}
+
+
+
+public class Civitas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
