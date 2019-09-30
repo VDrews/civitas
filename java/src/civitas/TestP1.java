bffa74080e3a829ae35f0509524715bb8790add5
chemooon <jmpobma@hotmail.com>
2019-09-30 18:34:57 +0200

    Programa test a completar

--- java/src/civitas/TestP1.java
+++ java/src/civitas/TestP1.java
@@ -1,29 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package civitas;
 
-enum TipoCasilla {CALLE, SORPRESA, JUEZ, IMPUESTO, DESCANSO}
-enum TipoSorpresa {IRCARCEL, IRCASILLA, PAGARCOBRAR, PORCASAHOTEL, PORJUGADOR, SALIRCARCEL}
-enum EstadosJuego {
-  INICIO_TURNO,
-  DESPUES_CARCEL,
-  DESPUES_AVANZAR,
-  DESPUES_COMPRAR,
-  DESPUES_GESTIONAR
-}
 
-
-
-public class Civitas {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class TestP1{
+      public static void main() {
+        //Aquí el programa de prueba
+      }
     
 }
