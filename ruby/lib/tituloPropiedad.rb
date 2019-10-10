1fbd79528b6bad6d15cac822e542fe6391760e70
chemooon <jmpobma@hotmail.com>
2019-10-10 18:19:14 +0200

    Sorpresa/TituloPropiedad terminados. Nombres

--- ruby/lib/tituloPropiedad.rb
+++ ruby/lib/tituloPropiedad.rb
@@ -1,30 +1,26 @@
 module Civitas
 
     class TituloPropiedad
+
+        attr_reader :hipotecado, :hipotecaBase, :nombre, :numCasas, :numHoteles, :precioCompra, :precioEdificar, :propietario
         
         private
         @@factorInteresesHipoteca=1.1
 
         def esEsteElPropietario(jugador)
-            
-        end
-
-        def getImporteHipoteca
-
+            return @propietario==jugador
         end
 
         def getPrecioAlquiler
             if (!@hipotecado || propietarioEncarcelado)
                 return (@alquilerBase*(1+(@numCasas*0.5)+(numHoteles*2.5)))
-            else return 0
-        end
-
-        def getPrecioVenta
-            
+            else 
+                return 0
+            end
         end
 
         def propietarioEncarcelado
-
+            return (tienePropietario&&!@propietario.isEncarcelado)
         end
         
         public
@@ -38,6 +34,7 @@ module Civitas
             @numHoteles=0
             @precioCompra=pc
             @precioEdificar=pe
+            @propietario=nil
         end
 
         def toString
@@ -69,47 +66,86 @@ module Civitas
         end
 
         def actualizaPropietarioPorConversion(jugador)
-            
+            @propietario=jugador
         end
 
         def cantidadCasasHoteles
-            
+            return @numCasas+@numHoteles
         end
 
         def comprar(jugador)
-            
+            if @propietario==nil
+                actualizaPropietarioPorConversion(jugador)
+                jugador.paga(@precioCompra)
+                return true
+            else
+                return false
+            end
         end
 
         def construirCasa(jugador)
-            
+            if(esEsteElPropietario(jugador))
+                @numCasas++
+                return true
+            else
+                return false
+            end
         end
 
         def construirHotel(jugador)
-            
+            if(esEsteElPropietario(jugador))
+                @numHoteles++
+                return true
+            else
+                return false
+            end
         end
 
         def derruirCasas(n, jugador)
-            
+            if(esEsteElPropietario(jugador)&&@numCasas>=n)
+                @numCasas-=n
+                return true
+            else
+                return false
+            end
         end
 
         def hipotecar(jugador)
-            if (!@hipotecado && )
-                
+            if (!@hipotecado && esEsteElPropietario(jugador))
+                jugador.recibe(@hipotecaBase)
+                @hipotecado=false
+                return true
             else
-                
+                return false
             end
         end
 
         def tienePropietario(jugador)
-
+            return @propietario!=nil
         end
 
         def tramitarAlquiler(jugador)
-
+            if (!@hipotecado && !esEsteElPropietario(jugador))
+                jugador.pagaAlquiler(getPrecioAlquiler)
+                @propietario.recibe(getPrecioAlquiler)
+                return true
+            else
+                return false
+            end
         end
 
         def vender(jugador)
-        
+            if (!@hipotecado && !esEsteElPropietario(jugador))
+                @propietario.recibe(getPrecioVenta)
+                jugador.paga(getPrecioVenta)
+                @propietario=jugador
+                return true
+            else
+                return false
+            end
         end
+
+        def getPrecioVenta
+            return @precioCompra+(@precioEdificar*cantidadCasasHoteles)*@factorRevalorizacion
     end
 end
