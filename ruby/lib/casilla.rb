2e5548cc3d9ec80edd836c94dc948633baf224ca
chemooon <jmpobma@hotmail.com>
2019-10-11 17:02:37 +0200

    Casilla

--- ruby/lib/casilla.rb
+++ ruby/lib/casilla.rb
@@ -3,20 +3,37 @@ module Civitas
         
         @@carcel
 
-        def initialize
-            
+        def initialize(importe, nombre, tipo, tituloPropiedad, mazo, sorpresa)
+            @importe=importe
+            @nombre=nombre
+            @tipo=tipo
+            @tituloPropiedad=tituloPropiedad
+            @mazo=mazo
+            @sorpresa=sorpresa
         end
+
+        attr_reader :nombre, :tituloPropiedad
+
+        def self.newDescanso(nombre)
+            new(nil, nombre, DESCANSO, nil, nil, nil)
+        end
+
+        def self.newCalle(titulo)
+            new(titulo.precioCompra, titulo.nombre, CALLE, titulo, nil, nil)            
+        end
+
+        def self.newImpuesto(cantidad, nombre)
+            new(cantidad, nombre, IMPUESTO, nil, nil. nil)
+        end
+
+        def self.newJuez(numCasillaCarcel, nombre)
+            new()
+        end
+
+        def self.newSorpresa(mazo, nombre)
 
         def informe
 
-        end
-
-        def recibeJugador_impuesto(actual, todos)
-            
-        end
-
-        def recibeJugador_juez(actual, todos)
-            
         end
 
         def toString
@@ -25,6 +42,27 @@ module Civitas
 
         def jugadorCorrecto(actual, todos)
 
+        end
+
+        def recibeJugador(actual, todos)
+
+        end
+
+        private
+        def recibeJugador_impuesto(actual, todos)
+            
+        end
+
+        def recibeJugador_calle(actual, todos)
+            
+        end
+
+        def recibeJugador_juez(actual, todos)
+            
+        end
+
+        def recibeJugador_sprpresa(actual, todos)
+            
         end
     end
 end
---
+++ ruby/lib/tipoCasilla.rb
@@ -0,0 +1,9 @@
+module Civitas
+    module TipoCasilla
+        SORPRESA=:sorpresa
+        CALLE=:calle
+        DESCANSO=:descanso
+        JUEZ=:juez
+        IMPUESTO=:impuesto
+    end
+end
